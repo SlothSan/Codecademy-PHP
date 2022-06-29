000d5482a22ffa58bf8566d64d7aac58c90dbcef
SlothSan <mikeoram01@gmail.com>
2022-06-29 10:55:58 +0100

    Changes for function

--- Mad_Lib_Functions/index.php
+++ Mad_Lib_Functions/index.php
@@ -1,9 +1,6 @@
 <?php
 
 function generateStory($singular_noun, $verb, $color) {
-    $story = "\nThe woods are lovely, dark and deep.\n
-                But I have promises to keep,\n
-                And miles to go before I sleep,\n
-                And miles to go before I sleep.\n";
+    $story = "\nThe ${singular_noun}s are lovely, $color and deep.\nBut I have promises to keep,\nAnd miles to go before I $verb,\nAnd miles to go before I $verb.\n";
     return $story;
 }
