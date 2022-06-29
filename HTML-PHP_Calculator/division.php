aa3971d45c88f298414030d87ea97103a5fd608a
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2022-06-29 16:28:19 +0100

    Update division.php

--- HTML-PHP_Calculator/division.php
+++ HTML-PHP_Calculator/division.php
@@ -1,7 +1,9 @@
 <html>
 <body>
 <!--Code goes here !-->
-<?php print_r($_GET)?>
+<?="${_GET['div_num']} divided by ${_GET['div_den']} is:"?>
+<br>
+<h4><?=$_GET["div_num"] / $_GET["div_den"]?></h4>
 <a href="./index.php">Reset</a>
 </body>
 </html>
