a92866995b44b9610d6951718c67ed60cea3b5ee
SlothSan <mikeoram01@gmail.com>
2022-06-29 10:56:39 +0100

    Testing

--- Mad_Lib_Functions/index.php
+++ Mad_Lib_Functions/index.php
@@ -4,3 +4,5 @@ function generateStory($singular_noun, $verb, $color) {
     $story = "\nThe ${singular_noun}s are lovely, $color and deep.\nBut I have promises to keep,\nAnd miles to go before I $verb,\nAnd miles to go before I $verb.\n";
     return $story;
 }
+
+echo generateStory("books", "finish", "biege");
