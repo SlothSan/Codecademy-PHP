857881283f4fe61071b90a45788261eb150630c6
SlothSan <mikeoram01@gmail.com>
2022-06-29 14:03:40 +0100

    Task 3

--- The_Hitchhikers_Guide_To_PHP_Math_Functions/index.php
+++ The_Hitchhikers_Guide_To_PHP_Math_Functions/index.php
@@ -1,11 +1,16 @@
 <?php
 // I am sorry Douglas Adams. 
 $initial = '555';
-
-$a = octdec($intial);
+//To dec number
+$a = octdec($initial);
 
 echo "$a \n";
-
+//degrees to Radian. 
 $b = deg2rad($a);
 
 echo "$b \n";
+
+//Cosine of $b
+$c = cos($b);
+
+echo "$c \n";
