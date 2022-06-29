75c9f771a245493b5c346509c517c3d331c9344f
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2022-06-29 15:36:43 +0100

    Update index.php

--- Bobs_Budget/index.php
+++ Bobs_Budget/index.php
@@ -11,9 +11,58 @@ $monthlyExpenses = [
     "healthInsurance" => 200
 ];
 
+$weeklyExpenses = [
+    "gas" => 25,
+    "food" => 90,
+    "entertainment" => 47
+];
+
 $grossSalary = 48150;
 $socialSecurity = $grossSalary * .062;
 
 $incomeSegments = [[9700, .88], [29775, .78], [8675, .76]];
 
 // Write your code below:
+
+//Annual Expenses
+$netIncome = 
+($incomeSegments[0][0] * $incomeSegments[0][1]) + 
+($incomeSegments[1][0] * $incomeSegments[1][1]) +
+($incomeSegments[2][0] * $incomeSegments[2][1]);
+
+$annualIncome = $netIncome - $socialSecurity;
+
+
+echo "Annual income before deducting annual expenses: \n$$annualIncome\n";
+
+$annualIncome -= $annualExpenses["vacations"];
+$annualIncome -= $annualExpenses["carRepairs"];
+
+echo "Annual income after deducting annual expenses: \n$$annualIncome\n";
+
+//Monthly Expenses
+ $monthlyIncome = round($annualIncome / 12, 2); 
+
+ echo "Monthly income before deducting monthly expenses: \n$$monthlyIncome\n";
+
+ $monthlyIncome -= $monthlyExpenses["rent"];
+ $monthlyIncome -= $monthlyExpenses["utilities"];
+ $monthlyIncome -= $monthlyExpenses["healthInsurance"];
+
+ echo "Monthly income after deducting monthly expenses: \n$$monthlyIncome\n";
+
+//Weekly Expenses
+$weeklyIncome = round($monthlyIncome / 4.33, 2);
+
+echo "Weekly income before deducting weekly expenses: \n$$weeklyIncome\n";
+
+$weeklyIncome -= $weeklyExpenses["gas"];
+$weeklyIncome -= $weeklyExpenses["food"];
+$weeklyIncome -= $weeklyExpenses["entertainment"];
+
+echo "Weekly income after deducting weekly expenses \n$$weeklyIncome\n";
+
+//Savings
+$savings = $weeklyIncome * 52;
+
+echo "Savings per year: \n$$savings\n";
