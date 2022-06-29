0a1c9b994c369011e6ad07effe2c97dea395866e
SlothSan <mikeoram01@gmail.com>
2022-06-29 15:52:10 +0100

    Index setup.

--- HTML-PHP_Calculator/index.php
+++ HTML-PHP_Calculator/index.php
@@ -1,7 +1,18 @@
 <html>
 <body>
 <!--Code goes here -->
-
+<h3>Addition</h3>
+<form>
+    First Number: <input type="number"><br>
+    Second Number: <input type="number"><br>
+    <button type="submit">Add</button>
+</form>
+<h3>Division</h3>
+<form>
+    Numerator: <input type="number"><br>
+    Denominator: <input type="number"><br>
+    <button type="submit">Divide</button>
+</form>
 <a href="./index.php">Reset</a>
 </body>
 </html>
