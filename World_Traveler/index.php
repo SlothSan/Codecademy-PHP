969be6ae038f67b9d909448ce7ef003ad30cc87b
SlothSan <mikeoram01@gmail.com>
2022-06-29 10:34:07 +0100

    Project finished

--- World_Traveler/index.php
+++ World_Traveler/index.php
@@ -5,7 +5,7 @@ $kyat = 19092;
 $krones = 109;
 $lek = 9094;
 
-echo "At the start of the transaction you had the following: $riel - riel, $kyat - kyat, $krones - krones and $lek - lek.";
+echo "At the start of the transaction you had the following: $riel - riel, $kyat - kyat, $krones - krones and $lek - lek.\n";
 
 //Exchange rates to USD.
 $riel_to_usd = 0.00024;
@@ -14,4 +14,14 @@ $krones_to_usd = 0.10;
 $lek_to_usd = 0.0088;
 
 $usd_from_riel = $riel * $riel_to_usd;
-$usd_from_kyat = 
+echo "Your riel was exchanged for $usd_from_riel USD.\n";
+$usd_from_kyat = $kyat * $kyat_to_usd;
+echo "Your kyat was exchanged for $usd_from_kyat USD.\n";
+$usd_from_krones = $krones * $krones_to_usd;
+echo "Your krones was exchanged for $usd_from_krones USD.\n";
+$usd_from_lek = $lek * $lek_to_usd;
+echo "Your lek was exchanged for $usd_from_lek USD.\n";
+
+
+$final_amount = $usd_from_riel + $usd_from_kyat + $usd_from_krones + $usd_from_lek - 4;
+echo "After deducting the transaction fee ($1 per transaction), you will be recieving $final_amount USD."
