41423bacff7578f47348e9650abb7a8ec4798ee1
SlothSan <mikeoram01@gmail.com>
2022-06-29 11:17:08 +0100

    More changes

--- Mad_Lib_Functions/index.php
+++ Mad_Lib_Functions/index.php
@@ -1,12 +1,12 @@
 <?php
 
-function generateStory($singular_noun, $verb, $color) {
-    $story = "\nThe ${singular_noun}s are lovely, $color and deep.\nBut I have promises to keep,\nAnd miles to go before I $verb,\nAnd miles to go before I $verb.\n";
+function generateStory($singular_noun, $verb, $color, $distance_unit) {
+    $story = "\nThe ${singular_noun}s are lovely, $color and deep.\nBut I have promises to keep,\nAnd $distance_unit to go before I $verb,\nAnd $distance_unit to go before I $verb.\n";
     return $story;
 }
 
-echo generateStory("book", "finish", "biege");
-echo "\n"
-echo generateStory("city", "see", "grey")
-echo "\n"
-echo generateStory("sea", "break", "blue")
+echo generateStory("book", "finish", "biege", "pages");
+echo "\n";
+echo generateStory("city", "see", "grey", "miles");
+echo "\n";
+echo generateStory("sea", "break", "blue", "fathoms");
