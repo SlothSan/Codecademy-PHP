700a1b323ea54309101f159e3bbb019da0403437
SlothSan <mikeoram01@gmail.com>
2022-06-29 15:47:05 +0100

    Initial commit

---
+++ HTML-PHP_Calculator/addition.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<!--Code goes here !-->
+
+<a href="./index.php">Reset</a>
+</body>
+</html>
---
+++ HTML-PHP_Calculator/index.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<!--Code goes here -->
+
+<a href="./index.php">Reset</a>
+</body>
+</html>
+
+<!--
+<html>
+<body>
+Code goes here
+<a href="./index.php">Reset</a>
+</body>
+</html>
+-->
