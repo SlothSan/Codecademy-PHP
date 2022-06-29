48c7d0134e093ac0974087ca6603ed4cfd02e80a
SlothSan <mikeoram01@gmail.com>
2022-06-29 10:51:05 +0100

    Initial commit.

--- Mad_Lib_Functions/index.php
+++ Mad_Lib_Functions/index.php
@@ -1,2 +1,9 @@
 <?php
 
+function generateStory($singular_noun, $verb, $color) {
+    $story = "\nThe woods are lovely, dark and deep.\n
+                But I have promises to keep,\n
+                And miles to go before I sleep,\n
+                And miles to go before I sleep.\n";
+    return $story;
+}
