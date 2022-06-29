6eda484bf2f8761e171e1e3d2666b4586a862714
SlothSan <mikeoram01@gmail.com>
2022-06-29 11:20:44 +0100

    Few more changes to formatting.

--- Mad_Lib_Functions/index.php
+++ Mad_Lib_Functions/index.php
@@ -1,7 +1,7 @@
 <?php
 
 function generateStory($singular_noun, $verb, $color, $distance_unit) {
-    $story = "\nThe ${singular_noun}s are lovely, $color and deep.\nBut I have promises to keep,\nAnd $distance_unit to go before I $verb,\nAnd $distance_unit to go before I $verb.\n";
+    $story = "\nThe ${singular_noun}s are lovely, ${color} and deep.\nBut I have promises to keep,\nAnd ${distance_unit} to go before I ${verb},\nAnd ${distance_unit} to go before I ${verb}.\n";
     return $story;
 }
 
